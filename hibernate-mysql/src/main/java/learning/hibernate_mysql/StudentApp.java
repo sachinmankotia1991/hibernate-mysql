f75357cac3aa7b269d551cdc0b988a25e8710cde
smankoti <smankoti@PPC18670.prod.telenet.be>
2022-03-25 10:51:27 +0100

    fix

--- hibernate-mysql/src/main/java/learning/hibernate_mysql/StudentApp.java
+++ hibernate-mysql/src/main/java/learning/hibernate_mysql/StudentApp.java
@@ -15,7 +15,7 @@ public class StudentApp {
 		Configuration cfg = new Configuration();
 		cfg.configure("hibernate.cfg.xml");
 		SessionFactory factory = cfg.buildSessionFactory();
-		
+
 		System.out.println("Hibernate SAVE method started");
 		Student student = new Student();
 		student.setFirstName("Naman");
@@ -23,7 +23,8 @@ public class StudentApp {
 		student.setGender('F');
 		student.setDob(new Date(92, 05, 05));
 		// reading and adding image to student object
-		FileInputStream fis = new FileInputStream("C:\\Users\\smankoti\\GIT\\hibernate-mysql\\src\\main\\java\\learning\\hibernate_mysql\\profile-pic.jpg");
+		FileInputStream fis = new FileInputStream(
+				"C:\\Users\\smankoti\\GIT\\hibernate-mysql\\src\\main\\java\\learning\\hibernate_mysql\\profile-pic.jpg");
 		byte[] data = new byte[fis.available()];
 		fis.read(data);
 		student.setImage(data);
@@ -31,22 +32,19 @@ public class StudentApp {
 		Transaction tx = session.beginTransaction();
 		session.save(student);
 		tx.commit();
-		
+
 		System.out.println("---------------------------------------------");
 		// GET method do eager loading
 		System.out.println("Hibernate GET method started");
 		Student studentGet = session.get(Student.class, 2);
-		System.out.println("Printing GET student : "+studentGet);
-		
-		
-		
+		System.out.println("Printing GET student : " + studentGet);
+
 		System.out.println("---------------------------------------------");
 		// LOAD method do eager loading
 		System.out.println("Hibernate LOAD method started");
 		Student loadStudent = session.load(Student.class, 1);
-		System.out.println("Printing LOAD student : "+loadStudent);
-		
-		
+		System.out.println("Printing LOAD student : " + loadStudent);
+
 		session.close();
 		factory.close();
 
